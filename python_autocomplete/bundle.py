6ab0b1d7618b4a26e4d59242278432dff0ccbc92
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-02-16 15:43:43 +0530

    bundle

--- python_autocomplete/bundle.py
+++ python_autocomplete/bundle.py
@@ -1,5 +1,9 @@
 from labml import experiment, lab
 
 if __name__ == '__main__':
-    experiment.save_bundle(lab.get_path() / 'bundle.tar.gz', '39b03a1e454011ebbaff2b26e3148b3d',
-                           data_files=['cache/itos.json', 'cache/n_tokens.json', 'cache/stoi.json'])
+    experiment.save_bundle(lab.get_path() / 'bundle.tar.gz', 'a6cff3706ec411ebadd9bf753b33bae6',
+                           data_files=['cache/itos.json',
+                                       'cache/n_tokens.json',
+                                       'cache/stoi.json',
+                                       'cache/bpe.json',
+                                       ])
